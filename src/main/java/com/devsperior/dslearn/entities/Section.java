72b90794b87f78aa111cc114367577bc6fa52805
Adiel Dantas <adieldantasn@gmail.com>
2025-07-09 11:50:41 -0300

    Lesson, Content, Task

--- src/main/java/com/devsperior/dslearn/entities/Section.java
+++ src/main/java/com/devsperior/dslearn/entities/Section.java
@@ -2,6 +2,8 @@ package com.devsperior.dslearn.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -23,6 +25,9 @@ public class Section {
     @ManyToOne
     @JoinColumn(name = "prerequisite_id")
     private Section prerequisite;
+
+    @OneToMany(mappedBy = "section")
+    private List<Lesson> lessons = new ArrayList<>();
 
     public Section() {
     }
