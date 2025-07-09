d2ca53ef3be5fea735201f3fbf989dd68cef9419
Adiel Dantas <adieldantasn@gmail.com>
2025-07-09 11:54:05 -0300

    Enrollment entity update

--- src/main/java/com/devsperior/dslearn/entities/Enrollment.java
+++ src/main/java/com/devsperior/dslearn/entities/Enrollment.java
@@ -1,13 +1,12 @@
 package com.devsperior.dslearn.entities;
 
 import com.devsperior.dslearn.entities.pk.EnrollmentPK;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -23,6 +22,9 @@ public class Enrollment {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {
     }
